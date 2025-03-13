3827861e33101bace8a2e646dbbf82e9a1bbba68
Rufus Barnes <rufus.barnes@amadeus.com>
2025-03-13 17:14:05 +0000

    feat: mve

--- src/js/capacitor-welcome.js
+++ src/js/capacitor-welcome.js
@@ -1,46 +1,40 @@
-import { Browser } from "@capacitor/browser";
 import { SplashScreen } from "@capacitor/splash-screen";
+import { BarcodeScanner } from "@capacitor-mlkit/barcode-scanning";
 
 window.customElements.define(
-  "capacitor-welcome",
-  class extends HTMLElement {
-    constructor() {
-      super();
+    "capacitor-welcome",
+    class extends HTMLElement {
+        constructor() {
+            super();
+            SplashScreen.hide();
+            const root = this.attachShadow({ mode: "open" });
+            root.innerHTML = `
+              <main>
+                <h1>Capacitor App</h1>
+                <button id="scanButton">Scan</button>
+                <button id="closeButton" style="visibility: visible">Close</button>
+              </main>
+            `;
+            this.shadowRoot.getElementById("scanButton").onclick = () => {
+                this.startScan();
+            };
+            this.shadowRoot.getElementById("closeButton").onclick = () => {
+                this.stopScan();
+            };
+        }
 
-      SplashScreen.hide();
+        async startScan() {
+            document.querySelector("body")?.classList.add("barcode-scanner-active");
+            await BarcodeScanner.addListener("barcodesScanned", async (result) => {
+                console.log(result);
+            });
 
-      const root = this.attachShadow({ mode: "open" });
-      root.innerHTML = `
-    <main>
-      <h1>Capacitor App</h1>
-      <p>
-        This project is used to create a minimal, reproducible example. Just add
-        the affected Capacitor platforms and plugins.
-      </p>
-      <label for="myInput">Website:</label>
-      <input
-        type="text"
-        id="myInput"
-        name="myInput"
-        value="https://capacitorjs.com/"
-      />
-      <button id="open-browser">Open Browser</button>
-    </main>
-    `;
+            await BarcodeScanner.startScan({ lensFacing: "BACK", formats: ["QR_CODE", "AZTEC", "DATA_MATRIX", "PDF_417"] });
+        }
+
+        async stopScan() {
+            document.querySelector("body")?.classList.remove("barcode-scanner-active");
+            await BarcodeScanner.stopScan();
+        }
     }
-
-    connectedCallback() {
-      const self = this;
-
-      self.shadowRoot
-        .querySelector("#open-browser")
-        .addEventListener("click", async function (event) {
-          const input = self.shadowRoot.getElementById("myInput").value;
-          if (!input) {
-            return;
-          }
-          await Browser.open({ url: input });
-        });
-    }
-  },
 );
